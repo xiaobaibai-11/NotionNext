9e7c0ddb93f45009ecc1de4327ee2343bb379812
xiaobaibai-11 <372171195@qq.com>
2025-03-27 16:51:07 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://www.zsshare.top/' },
-    { title: '点我教你搞钱，最新最全的网络副业项目', url: 'https://gpt.mlover.site/' }
+    { title: '点我教你搞钱，最新最全的网络副业项目', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
